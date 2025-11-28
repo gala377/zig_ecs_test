f3792462492f8f6d620e6d4ff81a88e723f6e875
gala377 <r.galczak@gmail.com>
2025-11-28 19:10:36 +0100

    making it towards creating components from lua

--- ecs/comp_vtable_storage.zig
+++ ecs/comp_vtable_storage.zig
@@ -2,6 +2,7 @@ const ComponentWrapper = @import("entity_storage.zig").ComponentWrapper;
 const std = @import("std");
 
 allocator: std.mem.Allocator,
+name_to_id: std.StringHashMap(u64),
 vtables: std.AutoHashMap(u64, *ComponentWrapper.VTable),
 
 const Self = @This();
@@ -9,6 +10,7 @@ const Self = @This();
 pub fn init(allocator: std.mem.Allocator) Self {
     return .{
         .allocator = allocator,
+        .name_to_id = .init(allocator),
         .vtables = .init(allocator),
     };
 }
@@ -21,6 +23,8 @@ pub fn new(self: *Self, comp_id: u64, vtable: ComponentWrapper.VTable) !*Compone
     const mem = try self.allocator.create(ComponentWrapper.VTable);
     mem.* = vtable;
     try self.vtables.put(comp_id, mem);
+    const name = try self.allocator.dupe(u8, vtable.name);
+    try self.name_to_id.put(name, comp_id);
     return self.vtables.get(comp_id).?;
 }
 
@@ -29,5 +33,10 @@ pub fn deinit(self: *Self) void {
     while (values.next()) |vtable| {
         self.allocator.destroy(vtable.*);
     }
+    var names = self.name_to_id.keyIterator();
+    while (names.next()) |name| {
+        self.allocator.free(name.*);
+    }
     self.vtables.deinit();
+    self.name_to_id.deinit();
 }
